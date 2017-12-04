6f0dd10bec5adac1746e2d65547e1fcc06e6a60c
mmyuan <yuanke52014@sohu.com>
2017-12-04 18:19:29 +0800

    test commit

--- src/main/java/org/lsqt/Main.java
+++ src/main/java/org/lsqt/Main.java
@@ -3,6 +3,11 @@ package org.lsqt;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+/**
+ * 启动类
+ * @author admin
+ *
+ */
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
