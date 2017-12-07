c28e2168504a8682a846eee3e639531697c014e1
mmyuan <huajiao_tech@163.com>
2017-12-07 20:32:18 +0800

    解决冲突

--- src/main/java/org/lsqt/sys/controller/TenantController.java
+++ src/main/java/org/lsqt/sys/controller/TenantController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.lsqt.components.context.annotation.Controller;
 import org.lsqt.components.context.annotation.Inject;
 import org.lsqt.components.context.annotation.mvc.RequestMapping;
-<<<<<<< HEAD
 import org.lsqt.components.db.Db;
 import org.lsqt.components.db.Page;
 import org.lsqt.components.util.lang.StringUtil;
@@ -42,110 +41,5 @@ public class TenantController {
 		List<Long> list = StringUtil.split(Long.class, ids, ",");
 		return tenantService.deleteById(list.toArray(new Long[list.size()]));
 	}
-=======
-
-import org.lsqt.components.db.Db;
-import org.lsqt.components.db.Page;
-import org.lsqt.components.util.lang.StringUtil;
-import org.lsqt.sys.model.Dictionary;
-import org.lsqt.sys.model.Tenant;
-import org.lsqt.sys.model.TenantQuery;
-import org.lsqt.sys.service.TenantService;
-
-
-
-
-@Controller(mapping={"/tenant"})
-public class TenantController {
-	
-	@Inject private TenantService tenantService; 
-	
-	@Inject private Db db;
-	
-	@RequestMapping(mapping = { "/page", "/m/page" })
-	public Page<Tenant> queryForPage(TenantQuery query) throws IOException {
-		return tenantService.queryForPage(query); //  
-	}
-	
-	@RequestMapping(mapping = { "/all", "/m/all" })
-	public Collection<Tenant> getAll() {
-		return tenantService.getAll();
-	}
-	
-	@RequestMapping(mapping = { "/save_or_update", "/m/save_or_update" })
-	public Tenant saveOrUpdate(Tenant form) {
-		return tenantService.saveOrUpdate(form);
-	}
-	
-	@RequestMapping(mapping = { "/delete", "/m/delete" })
-	public int delete(String ids) {
-		List<Long> list = StringUtil.split(Long.class, ids, ",");
-		return tenantService.deleteById(list.toArray(new Long[list.size()]));
-	}
-	
-	
-	/** 暂时注释，未能提供通用实现！！！
-	@RequestMapping(mapping = { "/export", "/m/export" })
-	public void export(TenantQuery query, String exportFileType, String exportDataType) {
-		Page<Tenant> page = db.getEmptyPage();
-		
-		// 1.导出excel
-		if (Dictionary.EXPORT_FILE_TYPE_EXCEL.equals(exportFileType)) {
-			
-			if (Dictionary.EXPORT_DATA_TYPE_全部数据.equals(exportDataType)) {
-				
-				page.setData(tenantService.getAll());
-				ContextUtil.file.put("/template/应用列表数据.xls", "page", page);
-				return ;
-			} 
-			
-			page = tenantService.queryForPage(query);
-			ContextUtil.file.put("/template/应用列表数据.xls", "page", page);
-			
-			return;
-		}
-
-		// 2.导出文本文件
-		if (Dictionary.EXPORT_FILE_TYPE_TXT.equals(exportFileType)) {
-			if (Dictionary.EXPORT_DATA_TYPE_全部数据.equals(exportDataType)) {
-				page.setData(tenantService.getAll());
-				ContextUtil.file.put("/template/应用列表数据.txt", "page", page);
-				return;
-			}
-
-			page = tenantService.queryForPage(query);
-			ContextUtil.file.put("/template/应用列表数据.txt", "page", page);
-			return;
-		}
-		
-		// 3.导出doc文件
-		if (Dictionary.EXPORT_FILE_TYPE_DOC.equals(exportFileType)) {
-			if (Dictionary.EXPORT_DATA_TYPE_全部数据.equals(exportDataType)) {
-				page.setData(tenantService.getAll());
-				ContextUtil.file.put("/template/应用列表数据.doc", "page", page);
-				return ;
-			}
-			
-			page = tenantService.queryForPage(query);
-			ContextUtil.file.put("/template/应用列表数据.doc", "page", page);
-			return;
-		}
-
-		// 4.导出pdf文件
-		if (Dictionary.EXPORT_FILE_TYPE_PDF.equals(exportFileType)) {
-			if (Dictionary.EXPORT_DATA_TYPE_全部数据.equals(exportDataType)) {
-				page.setData(tenantService.getAll());
-				ContextUtil.file.put("/template/应用列表数据.pdf", "page", page);
-				return ;
-			}
-			
-			page = tenantService.queryForPage(query);
-			ContextUtil.file.put("/template/应用列表数据.pdf", "page", page);
-			return;
-		}
-		
-	}
-	**/
->>>>>>> branch 'master' of https://github.com/mmyuan1020/example.git
 	
 }
